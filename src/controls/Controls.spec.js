39cf11da5e26a5c27c8aa9a26ffcfd7c83bcddf0
Ronny Alvarado <rsalvarado777@gmail.com>
2019-06-25 18:56:40 -0500

    Made a 'renders without crashing' test for all 3 components

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,9 @@
-// Test away!
+import React from "react";
+import "@testing-library/react/cleanup-after-each";
+import "jest-dom/extend-expect";
+import Controls from "./Controls.js";
+import { render } from "@testing-library/react";
+
+it("renders without crashing", () => {
+  render(<Controls />);
+});
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,9 @@
-// Test away!
+import React from "react";
+import "@testing-library/react/cleanup-after-each";
+import "jest-dom/extend-expect";
+import Display from "./Display.js";
+import { render } from "@testing-library/react";
+
+it("renders without crashing", () => {
+  render(<Display />);
+});
